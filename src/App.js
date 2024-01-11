0f4220aed5bf7b05adc2b2fe2770d0ec77baa6dd
somemohit <98733767+somemohit@users.noreply.github.com>
2024-01-11 13:41:14 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { FaCircleCheck } from "react-icons/fa6";
+import "./App.css";
 
-function App() {
+export default function App() {
+  const [percentage, setPercentage] = useState(0);
+
+  useEffect(() => {
+    let interval;
+
+    if (percentage < 100) {
+      interval = setInterval(() => {
+        setPercentage((val) => val + 1);
+      }, 50);
+    }
+
+    return () => clearInterval(interval);
+  }, [percentage]);
+
+  // console.log(percentage, "percentage");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <h1>Progress Bar</h1>
+      <div className="progress-bar-container">
+        <span
+          className="percentage-text"
+          style={percentage > 60 ? { color: "white" } : { color: "black" }}
         >
-          Learn React
-        </a>
-      </header>
+          {percentage}%
+        </span>
+        <div
+          style={{ width: `${percentage}%` }}
+          className="progress-tracker"
+        ></div>
+      </div>
+
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          height: "50px",
+        }}
+      >
+        {percentage === 100 ? (
+          <>
+            <p>Completed</p>
+            <FaCircleCheck style={{ color: "green", marginLeft: "5px" }} />
+          </>
+        ) : (
+          <p>Loading...</p>
+        )}
+      </div>
     </div>
   );
 }
-
-export default App;
